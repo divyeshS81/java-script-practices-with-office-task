02539cfdce4334a12bdcf52a042c187ba8842262
divyesh sarvaiya <divyeshsarvaiya999@gmail.com>
2024-02-08 12:19:37 +0530

    upload a promise example

--- dom in js/promises/main.js
+++ dom in js/promises/main.js
@@ -56,3 +56,17 @@ countValue.then(
                   console.log(result);
             }
       );*/
+
+
+/*var p = new Promise(function (resolve, reject) {
+      var x = 2 + 3;
+      if (x == 5)
+            resolve(" executed and resolved successfully");
+      else
+            reject("rejected");
+});
+p.then(function (fromResolve) {
+      document.write("Promise is" + fromResolve);
+}).catch(function (fromReject) {
+      document.write("Promise is " + fromReject);
+});  */
